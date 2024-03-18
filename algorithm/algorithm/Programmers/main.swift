9f4501f7866d443e94edad18160aaa92a69e4764
gnghl7556 <gnghl7556@naver.com>
2024-03-19 00:23:27 +0900

    첫번째 문제해결

---
+++ algorithm/algorithm/Programmers/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  algorithmStudy
+//
+//  Created by Ethan on 2024/03/18.
+//
+
+import Foundation
+
+func solution(_ n:Int)-> String{
+    return "\(n)"
+}
+
+print(solution(10))
+
+func solution1(_ n:Int)-> String{
+    return String(n)
+}
+print(solution1(10))
