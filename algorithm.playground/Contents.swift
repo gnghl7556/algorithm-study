feea9fde82f6dbede4b322da1b756c26bbcf0f99
gnghl7556 <gnghl7556@naver.com>
2024-03-19 15:49:53 +0900

    003. 문자열 앞의 n글자 - 문자열의 index 이해하기

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -14,4 +14,15 @@ func solution002(_ a:Int,_ b:Int)->Int {
 }
 print(solution002(2, 91))
 print(solution002(91, 2))
+/// 003 문자열의 앞의 n글자
 
+func solution003(_ my_string: String, _ n:Int) -> String {
+    return String(my_string.prefix(n)) // 앞에서부터 n글자까지~~ prefix(n) -> SubString 반드시 String으로 고쳐야함
+}
+print(solution003("ProgrammerS123", 11))
+func solution003_1(_ my_string: String,_ n:Int) -> String {
+    let index = my_string.index(my_string.startIndex, offsetBy: n)
+    let subString = my_string[my_string.startIndex..<index]
+    return String(subString)
+}
+print(solution003_1("ProgrammerS123", 11))
